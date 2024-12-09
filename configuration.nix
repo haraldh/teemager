c583133b9fc97d8e61e68475c4a4b3b7eab318cd
Harald Hoyer <harald@hoyer.xyz>
2024-12-09 10:39:07 +0100

    Add system packages and improve network settings
    This commit adds essential system packages such as `tdx_attest` and `openssl`, and updates environment variables for SGX configuration. SSH access is enabled with root login permitted using authorized keys, enhancing remote accessibility and security. The network settings have also been improved to allow SSH connections and enable networkd by default. Various boot and user configurations were modified to improve initialization and system security settings.

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,7 @@
 {
   lib,
   modulesPath,
+  pkgs,
   ...
 }: {
   imports = [
@@ -9,30 +10,70 @@
     #./amazon.nix
   ];
 
+  environment.systemPackages = with pkgs; [
+    tdx_attest
+    teepot.teepot.tee_self_attestation_test
+    openssl
+    curl
+    nixsgx.sgx-dcap.quote_verify
+    nixsgx.sgx-dcap.default_qpl
+  ];
+
+  environment.variables = {
+    QCNL_CONF_PATH = "${pkgs.nixsgx.sgx-dcap.default_qpl}/etc/sgx_default_qcnl.conf";
+  };
+
   system.image.id = "nixos-appliance-test";
 
-  #services.openssh.settings.PermitRootLogin = lib.mkOverride 999 "yes";
   boot.enableContainers = lib.mkDefault false;
   boot.initrd.systemd.enable = lib.mkDefault true;
-  boot.kernelParams = ["console=tty0"];
+  boot.kernelParams = [
+    "console=ttyS0,115200n8"
+  ];
+  boot.initrd.availableKernelModules = [
+    "tdx_guest"
+  ];
 
-  documentation.info.enable = lib.mkDefault false;
+  services.logind.extraConfig = ''
+    NAutoVTs=0
+    ReserveVT=0
+  '';
+  console.enable = false;
+
+  #services.getty.autologinUser = lib.mkOverride 999 "root";
+
+  boot.initrd.systemd.tpm2.enable = lib.mkForce false;
+  systemd.tpm2.enable = lib.mkForce false;
+
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+  documentation.info.enable = lib.mkForce false;
+  documentation.nixos.enable = lib.mkForce false;
+  documentation.man.enable = lib.mkForce false;
+  documentation.enable = lib.mkForce false;
 
   networking.useNetworkd = lib.mkDefault true;
+  networking.firewall.allowedTCPPorts = [22];
+  networking.firewall.allowPing = true;
 
   nix.enable = false;
 
   programs.command-not-found.enable = lib.mkDefault false;
   programs.less.lessopen = lib.mkDefault null;
 
-  services.getty.autologinUser = lib.mkOverride 999 "root";
-  services.sshd.enable = lib.mkForce false;
+  services.sshd.enable = true;
+  services.openssh.settings.PermitRootLogin = lib.mkOverride 999 "yes";
+
   services.udisks2.enable = false; # udisks has become too bloated to have in a headless system
 
-  system.disableInstallerTools = lib.mkDefault true;
+  system.disableInstallerTools = lib.mkForce true;
   system.stateVersion = lib.version;
-  system.switch.enable = lib.mkDefault false;
+  system.switch.enable = lib.mkForce false;
 
   users.mutableUsers = false;
-  users.users.root.password = "nixos";
+  users.users.root.openssh.authorizedKeys.keys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDsb/Tr69YN5MQLweWPuJaRGm+h2kOyxfD6sqKEDTIwoAAAABHNzaDo= harald@fedora.fritz.box"
+    "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBACLgT81iB1iWWVuXq6PdQ5GAAGhaZhSKnveQCvcNnAOZ5WKH80bZShKHyAYzrzbp8IGwLWJcZQ7TqRK+qZdfagAAAAEc3NoOg== harald@hoyer.xyz"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAYbUTKpy4QR3s944/hjJ1UK05asFEs/SmWeUbtS0cdA660sT4xHnRfals73FicOoz+uIucJCwn/SCM804j+wtM="
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNsmP15vH8BVKo7bdvIiiEjiQboPGcRPqJK0+bH4jKD harald@lenovo.fritz.box"
+  ];
 }
