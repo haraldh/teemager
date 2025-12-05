689d03bbe3c2eef8e76d2b5a5880d1b20f2dd95f
Harald Hoyer <harald@subzero.xyz>
2025-12-05 13:24:35 +0100

    feat: add EC2 metadata fetcher service to AWS configuration
    - add `fetch-ec2-metadata` systemd service with dependencies and script integration
    - ensure the service outputs metadata to journal+console
    - retain EC2 NTP server configuration

--- image/aws.nix
+++ image/aws.nix
@@ -1,7 +1,19 @@
 {
+  pkgs,
+  modulesPath,
   ...
 }:
 {
-    # EC2 has its own NTP server provided by the hypervisor
-    networking.timeServers = ["169.254.169.123"];
+  systemd.services.fetch-ec2-metadata = {
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    path = [ pkgs.curl ];
+    script = builtins.readFile "${toString modulesPath}/virtualisation/ec2-metadata-fetcher.sh";
+    serviceConfig.Type = "oneshot";
+    serviceConfig.StandardOutput = "journal+console";
+  };
+
+  # EC2 has its own NTP server provided by the hypervisor
+  networking.timeServers = [ "169.254.169.123" ];
 }
